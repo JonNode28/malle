63897087b41247e880f65833498fe184e116be46
Jonathan Pritchard <jon@node28.com>
2020-05-09 09:07:31 +0100

    export various modules from package

--- packages/renderer-component-library-react/src/index.ts
+++ packages/renderer-component-library-react/src/index.ts
@@ -1,8 +1,11 @@
-import { ListItem, createListItemRenderer } from './list-item-renderer'
+import ListItemRenderer from './list-item-renderer';
+import StringRenderer from './type-renderers/string';
 import Pagination from './pagination';
+import ErrorPanel from "./error-panel";
 
 export {
-  ListItem,
-  createListItemRenderer,
-  Pagination
+  ListItemRenderer,
+  Pagination,
+  StringRenderer,
+  ErrorPanel
 };
