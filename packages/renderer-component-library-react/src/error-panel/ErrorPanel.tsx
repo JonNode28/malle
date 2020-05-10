32d96731459c2d405d3e30c54d8becad96786777
Jonathan Pritchard <jon@node28.com>
2020-05-10 11:56:47 +0100

    Better handle errors

--- packages/renderer-component-library-react/src/error-panel/ErrorPanel.tsx
+++ packages/renderer-component-library-react/src/error-panel/ErrorPanel.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import s from './ErrorPanel.pcss';
 
-export default function ErrorPanel({ err }: any){
+interface ErrorPanelProps {
+  err: Error | string
+}
+
+export default function ErrorPanel({ err }: ErrorPanelProps){
+  let message: string = 'An error occurred';
+  if(typeof err === 'string') message = err;
+  else if('message' in err) message = err.message;
   return (
-    <div className={s.error} data-testid='error-panel'>{err?.message || 'An error occurred'}</div>
+    <div className={s.error} data-testid='error-panel'>{message}</div>
   )
 }
