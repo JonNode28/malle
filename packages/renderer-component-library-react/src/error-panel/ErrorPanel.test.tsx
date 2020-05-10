b74c42e615de44a6881e40005e2c08e7679da8b6
Jonathan Pritchard <jon@node28.com>
2020-05-10 19:51:31 +0100

    Error panel now handles string errors as well as Error objects

--- packages/renderer-component-library-react/src/error-panel/ErrorPanel.test.tsx
+++ packages/renderer-component-library-react/src/error-panel/ErrorPanel.test.tsx
@@ -4,18 +4,24 @@ import React from "react";
 import ErrorPanel from "./ErrorPanel";
 
 describe(`<ErrorPanel />`, () => {
-  it(`should display the error message`, () => {
-    render(<ErrorPanel err={{ message: 'some error message' }} />);
+  it(`should display an Error error message`, () => {
+    render(<ErrorPanel err={new Error('some Error error message' )} />);
+    const errorPanel = screen.getByTestId('error-panel');
+    expect(errorPanel.textContent).toMatchSnapshot();
+  });
+  it(`should display a string error message`, () => {
+    render(<ErrorPanel err={'some string error message'} />);
     const errorPanel = screen.getByTestId('error-panel');
     expect(errorPanel.textContent).toMatchSnapshot();
   });
   it(`should display a default message if no error is provided`, () => {
+    // @ts-ignore
     render(<ErrorPanel err={null} />);
     const errorPanel = screen.getByTestId('error-panel');
     expect(errorPanel.textContent).toMatchSnapshot();
   });
   it(`should display a default message if no error message is provided`, () => {
-    render(<ErrorPanel err={{}} />);
+    render(<ErrorPanel err={new Error()} />);
     const errorPanel = screen.getByTestId('error-panel');
     expect(errorPanel.textContent).toMatchSnapshot();
   });
--- packages/renderer-component-library-react/src/error-panel/ErrorPanel.tsx
+++ packages/renderer-component-library-react/src/error-panel/ErrorPanel.tsx
@@ -8,7 +8,7 @@ interface ErrorPanelProps {
 export default function ErrorPanel({ err }: ErrorPanelProps){
   let message: string = 'An error occurred';
   if(typeof err === 'string') message = err;
-  else if('message' in err) message = err.message;
+  else if(err && 'message' in err && err.message) message = err.message;
   return (
     <div className={s.error} data-testid='error-panel'>{message}</div>
   )
