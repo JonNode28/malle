5ad725beabd746b05c33c3eab8553c9c20071fdf
Jonathan Pritchard <jon@node28.com>
2020-05-19 16:50:39 +0100

    Render errors in string type renderer

--- packages/renderer-component-library-react/src/type-renderers/string/StringRenderer.tsx
+++ packages/renderer-component-library-react/src/type-renderers/string/StringRenderer.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import {TypeRendererProps} from "malle-renderer-react";
 import { PropertyTypeRendererProps } from "malle-renderer-react";
+import s from './StringRenderer.pcss'
 
-export default function StringRenderer({ data, propertyConfig, onChange }: PropertyTypeRendererProps){
+export default function StringRenderer({ data, propertyConfig, onChange, validationResults }: PropertyTypeRendererProps){
   if(!propertyConfig) throw Error(`propertyConfig argument is required`);
   if(!onChange) throw new Error(`onChange argument is required`);
   return (
     <div>
       <label htmlFor={propertyConfig.id}>{propertyConfig.name}</label>
-  {propertyConfig.description && <p>{propertyConfig.description}</p>}
-  <input
-    type='text'
-    id={propertyConfig.id}
-    value={data[propertyConfig.id]}
-    data-testid='string-input'
-    onChange={(e) => {
-      onChange && onChange({
-        ...data,
-        [propertyConfig.id]: e.currentTarget.value
-      });
-    }} />
-  </div>
+      {propertyConfig.description && <p>{propertyConfig.description}</p>}
+      <input
+        type='text'
+        id={propertyConfig.id}
+        value={data[propertyConfig.id]}
+        data-testid='string-input'
+        onChange={(e) => {
+          onChange && onChange({
+            ...data,
+            [propertyConfig.id]: e.currentTarget.value
+          });
+        }} />
+      {validationResults && validationResults.map((result, i) => (
+        <div className={s.error} key={i}>{result.errorMessage}</div>
+      ))}
+    </div>
   );
 }
