730cb6b09bc6321bbeaec041a5108d7eef985067
Jonathan Pritchard <jon@node28.com>
2020-05-19 16:44:55 +0100

    Added Validator interface

---
+++ packages/core/src/Validator.ts
@@ -0,0 +1,24 @@
+export interface Validator {
+  executeOn: Array<ValidationExecutionStage>
+}
+
+export interface ValidationResult {
+  valid: boolean
+  errorMessage?: string
+  errorDisplayMode?: Array<ValidationErrorDisplayMode>
+  dataPaths?: Array<string>
+}
+
+export enum ValidationExecutionStage {
+  CHANGE = 'CHANGE',
+  CLIENT_UPDATE = 'CLIENT_UPDATE',
+  CLIENT_CREATE = 'CLIENT_CREATE',
+  SERVER_UPDATE = 'SERVER_UPDATE',
+  SERVER_CREATE = 'SERVER_CREATE'
+}
+
+export enum ValidationErrorDisplayMode {
+  INLINE = 'INLINE',
+  SUMMARY = 'SUMMARY',
+  MODAL = 'MODAL'
+}
