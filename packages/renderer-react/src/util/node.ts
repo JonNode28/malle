8ad8b87ccb8c4ce091bfe453c39455c203d5f059
Jonathan Pritchard <jon@node28.com>
2020-10-03 20:32:00 +0100

    Fixed default value util

--- packages/renderer-react/src/util/node.ts
+++ packages/renderer-react/src/util/node.ts
@@ -6,6 +6,7 @@ export function createDefault(config: NodeConfig, fallbackValue: any = undefined
     if(typeof fallbackValue === 'undefined'){
       throw new Error(`A default value is required by the '${config.id}' model`)
     }
+    return fallbackValue
   } else if(defaultType === 'function'){
     return config.default()
   }
